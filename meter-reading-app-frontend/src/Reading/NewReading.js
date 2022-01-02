c6948106023e175bc2bab42be8282a70305c2b2e
davidsambucci <david.sambucci@vistair.com>
2022-01-02 19:51:01 +0000

    Adding imput Validation

--- meter-reading-app-frontend/src/Reading/NewReading.js
+++ meter-reading-app-frontend/src/Reading/NewReading.js
@@ -1,28 +1,52 @@
 import { useRef, useState } from "react";
 import "./NewReading.css";
 import { saveMeter } from "../ServerInterface/MeterInterface";
+import ErrorModal from '../UI/ErrorModal';
+
+
+function validate(meter){
+  if(meter.street.length <=0){
+    return error("Street mandatory", "The field Street is mandatory");
+  }
+  if(meter.streetNum.length <=0){
+    return error("House Number mandatory", "The House Number field is mandatory");
+  }
+  if(meter.energy.length <=0){
+    return error("Energy mandatory", "The field Energy is mandatory");
+  }
+}
+function error(title, message){
+  return {title, message};
+}
 
 const NewReading = (props) => {
   const [street, setStreet] = useState("");
   const [streetNum, setStreetNum] = useState("");
   const [energy, setEnergy] = useState("");
+  const [error, setError] = useState(null);
 
   function submitHandler(event) {
     event.preventDefault();
-    setStreet("");
-    setStreetNum("");
-    setEnergy("");
-    saveMeter({
+    const meter = {
       //TODO these variable names are not correct, we are putting those for now but they needs changing
       street,
       streetNum,
-      energy,
-    });
+      energy,};
+    const validationError = validate(meter);
+    if(validationError){
+      setError(validationError);
+      return;
+    }
+    setStreet("");
+    setStreetNum("");
+    setEnergy("");
+    saveMeter(meter);
     props.onSavedReading();
   }
-  function cancelHandler(){
+  function cancelHandler() {
     props.onCancel();
   }
+ 
 
   function streetChangeHandler(event) {
     setStreet(event.target.value);
@@ -34,34 +58,41 @@ const NewReading = (props) => {
     setEnergy(event.target.value);
   }
 
+function errorAcceptHandler(){
+  setError(null);
+}
+
   return (
-    <form className="new-reading" onSubmit={submitHandler}>
-      <div className="street-name">
-        <label>Street Name</label>
-        <input value={street} type="text" onChange={streetChangeHandler} />
-      </div>
-      <div className="street-num">
-        <label>Street Number</label>
-        <input
-          type="text"
-          value={streetNum}
-          onChange={streetNumChangeHandler}
-        />
-      </div>
-      <div className="street-energy">
-        <label>Energy</label>
-        <input
-          type="number"
-          value={energy}
-          onChange={energyChangeHandler}
-          step="1"
-        />
-      </div>
-      <div className="buttons">
-        <button onClick={cancelHandler}>Cancel</button>
-        <button type="submit">Save</button>
-      </div>
-    </form>
+    <div>
+      <form className="new-reading" onSubmit={submitHandler}>
+        <div className="street-name">
+          <label>Street Name</label>
+          <input value={street} type="text" onChange={streetChangeHandler} />
+        </div>
+        <div className="street-num">
+          <label>House Number</label>
+          <input
+            type="text"
+            value={streetNum}
+            onChange={streetNumChangeHandler}
+          />
+        </div>
+        <div className="street-energy">
+          <label>Energy</label>
+          <input
+            type="number"
+            value={energy}
+            onChange={energyChangeHandler}
+            step="1"
+          />
+        </div>
+        <div className="buttons">
+          <button onClick={cancelHandler}>Cancel</button>
+          <button type="submit">Save</button>
+        </div>
+      </form>
+      {error && <ErrorModal title={error.title} description={error.description} onAccept={errorAcceptHandler}/>}
+    </div>
   );
 };
 
---
+++ meter-reading-app-frontend/src/UI/ErrorModal.js
@@ -0,0 +1,25 @@
+import style from "./ErrorModal.module.css";
+import Button from "./Button";
+
+const ErrorModal = (props) => {
+  function clickHandler() {
+    props.onAccept();
+  }
+
+  return (
+    <div>
+      <div className={style.coverElement}></div>
+      <div className={style.errorModal}>
+        <header>
+          <h2>{props.title}</h2>
+        </header>
+        <div>{props.message}</div>
+        <footer>
+          <Button onClick={clickHandler}>Accept</Button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorModal;
