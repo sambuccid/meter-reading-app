858e98c3eab6ec19c294fbafca0ebf9e950460bc
sambuccid <sambuccid@gmail.com>
2022-01-22 18:33:21 +0000

    applying chnages from prev commit

--- meter-reading-app-frontend/src/ServerInterface/MeterInterface.js
+++ meter-reading-app-frontend/src/ServerInterface/MeterInterface.js
@@ -2,24 +2,38 @@
 //doesn't provide actual comunication with the server
 //but it is designed to support that later
 
+import * as ObjConverter from "./MeterObjectConverter";
+
 let lastId = 0;
 
 let meterList = [];
-function saveMeter(meter){
-    let id = meter.id;
-    if(id == null){
-        lastId++;
-        id=lastId;
-    }
 
-    //delayed to provide limits of async code, that will be needed when talking with server
-    setTimeout(()=>{meterList.push({...meter, id: id});},0);
+function saveMeter(meter) {
+  let id = meter.id;
+  if (id == null) {
+    lastId++;
+    id = lastId;
+  }
+
+  //delayed to provide limits of async code, that will be needed when talking with server
+  setTimeout(() => {
+    meterList.push({ ...ObjConverter.appToServer(meter), id: id });
+  }, 100);
+
+  fetch("/meter/new", {
+    method: "POST",
+    body: ObjConverter.appToServer(meter)
+  });
 }
 
-function allMeters(callback){
-    console.log("loading meters",meterList);
-    setTimeout(()=>{callback([...meterList]);},0);
+function allMeters() {
+  console.log(meterList);
+  const convertedMeterList = meterList.map((meter) =>
+    ObjConverter.serverToApp(meter)
+  );
+  return new Promise(function executor(resolve) {
+    setTimeout(() => resolve(convertedMeterList), 100);
+  });
 }
 
-
-export {saveMeter, allMeters};
+export { saveMeter, allMeters };
