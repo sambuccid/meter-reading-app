fff075ebcd25dc7f0210c91413558009d33c2885
sambuccid <sambuccid@gmail.com>
2022-01-22 18:31:10 +0000

    applying changed from previous commit to rightfile

--- meter-reading-app-frontend/src/Reading/NewReading.js
+++ meter-reading-app-frontend/src/Reading/NewReading.js
@@ -1,26 +1,28 @@
 import { useRef, useState } from "react";
 import "./NewReading.css";
 import { saveMeter } from "../ServerInterface/MeterInterface";
-import ErrorModal from '../UI/ErrorModal';
+import ErrorModal from "../UI/ErrorModal";
 
-
-function validate(meter){
-  if(meter.street.length <=0){
-    return error("Street mandatory", "The field Street is mandatory");
+function validate(meter) {
+  if (meter.postCode.length <= 0) {
+    return error("Post Code mandatory", "The field Post Code is mandatory");
   }
-  if(meter.streetNum.length <=0){
-    return error("House Number mandatory", "The House Number field is mandatory");
+  if (meter.streetNum.length <= 0) {
+    return error(
+      "House Number mandatory",
+      "The House Number field is mandatory"
+    );
   }
-  if(meter.energy.length <=0){
+  if (meter.energy.length <= 0) {
     return error("Energy mandatory", "The field Energy is mandatory");
   }
 }
-function error(title, message){
-  return {title, message};
+function error(title, message) {
+  return { title, message };
 }
 
 const NewReading = (props) => {
-  const streetRef = useRef();
+  const postCodeRef = useRef();
   const streetNumRef = useRef();
   const energyRef = useRef();
   const [error, setError] = useState(null);
@@ -28,19 +30,19 @@ const NewReading = (props) => {
   function submitHandler(event) {
     event.preventDefault();
     const meter = {
-      //TODO these variable names are not correct, we are putting those for now but they needs changing
-      street: streetRef.current.value,
+      postCode: postCodeRef.current.value,
       streetNum: streetNumRef.current.value,
-      energy: energyRef.current.value};
+      energy: energyRef.current.value
+    };
     const validationError = validate(meter);
-    if(validationError){
+    if (validationError) {
       setError(validationError);
       return;
     }
 
-    streetRef.current.value="";
-    streetNumRef.current.value="";
-    energyRef.current.value="";
+    postCodeRef.current.value = "";
+    streetNumRef.current.value = "";
+    energyRef.current.value = "";
 
     saveMeter(meter);
     props.onSavedReading();
@@ -49,38 +51,37 @@ const NewReading = (props) => {
     props.onCancel();
   }
 
-function errorAcceptHandler(){
-  setError(null);
-}
+  function errorAcceptHandler() {
+    setError(null);
+  }
 
   return (
     <>
       <form className="new-reading" onSubmit={submitHandler}>
-        <div className="street-name">
-          <label>Street Name</label>
-          <input ref={streetRef} type="text"/>
+        <div className="post-code">
+          <label>Post Code</label>
+          <input ref={postCodeRef} type="text" />
         </div>
         <div className="street-num">
           <label>House Number</label>
-          <input
-            type="text"
-            ref={streetNumRef}
-          />
+          <input type="text" ref={streetNumRef} />
         </div>
-        <div className="street-energy">
+        <div className="energy">
           <label>Energy</label>
-          <input
-            type="number"
-            step="1"
-            ref={energyRef}
-          />
+          <input type="number" step="1" ref={energyRef} />
         </div>
         <div className="buttons">
           <button onClick={cancelHandler}>Cancel</button>
           <button type="submit">Save</button>
         </div>
       </form>
-      {error && <ErrorModal title={error.title} message={error.message} onAccept={errorAcceptHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onAccept={errorAcceptHandler}
+        />
+      )}
     </>
   );
 };
