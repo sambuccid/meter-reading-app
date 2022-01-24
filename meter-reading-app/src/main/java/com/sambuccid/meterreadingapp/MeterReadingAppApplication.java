d1f5c832368e67379eddf090033608fe1cec5b3b
David <sambuccid@gmail.com>
2022-01-24 16:02:55 +0000

    Add config to allow CORS origin, just for development

--- meter-reading-app/src/main/java/com/sambuccid/meterreadingapp/MeterReadingAppApplication.java
+++ meter-reading-app/src/main/java/com/sambuccid/meterreadingapp/MeterReadingAppApplication.java
@@ -2,14 +2,27 @@ package com.sambuccid.meterreadingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MeterReadingAppApplication extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MeterReadingAppApplication.class, args);
+	}
+
+	//This is needed just for development purposes, once the front end will be compiled in this server we can remove this config
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
 	}
 
 }
