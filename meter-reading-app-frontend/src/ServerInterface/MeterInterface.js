ea7eec474ec2fb482030eecc3d233acf268f966f
David <sambuccid@gmail.com>
2022-01-24 16:05:03 +0000

    implementing conmunication with server
    it works once cors gets enabled in the server, of course cors won't be needed when we actually deploy the app
    as it would be served by the server that exposes the API

--- meter-reading-app-frontend/src/ServerInterface/MeterInterface.js
+++ meter-reading-app-frontend/src/ServerInterface/MeterInterface.js
@@ -1,44 +1,24 @@
-//simple initial interface
-//doesn't provide actual comunication with the server
-//but it is designed to support that later
-
 import * as ObjConverter from "./MeterObjectConverter";
 
-let lastId = 0;
-
-let meterList = [];
+//TODO this will be removed once the app will be served by spring boot itself
+const URL_ENDPOINT = "http://localhost:8080";
 
 function saveMeter(meter) {
-  let id = meter.id;
-  if (id == null) {
-    lastId++;
-    id = lastId;
-  }
-
-  //delayed to provide limits of async code, that will be needed when talking with server
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const newMeter = { ...meter, id: id };
-      console.log(newMeter);
-      meterList.push(ObjConverter.appToServer(newMeter));
-      resolve();
-    }, 100);
-  });
-  /*
-  await fetch("/meter/new", {
+  return fetch(URL_ENDPOINT + "/meters", {
     method: "POST",
-    body: ObjConverter.appToServer(meter)
-  });*/
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(ObjConverter.appToServer(meter))
+  });
 }
 
-function allMeters() {
-  console.log(meterList);
-  const convertedMeterList = meterList.map((meter) =>
-    ObjConverter.serverToApp(meter)
-  );
-  return new Promise(function executor(resolve) {
-    setTimeout(() => resolve(convertedMeterList), 100);
-  });
+async function allMeters(){
+  const resp = await fetch(URL_ENDPOINT + "/meters");
+  const metersResp = await resp.json();
+  console.log(metersResp);
+  const meters = metersResp.map((meterResp) => ObjConverter.serverToApp(meterResp));
+  return meters;
 }
 
 export { saveMeter, allMeters };
